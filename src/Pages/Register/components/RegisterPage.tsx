5f76580636bdd57bfb422f9b5eb89f3b1f627a8d
Bondar-Sasha <aliaksandr.bondar@innowise.com>
2025-01-16 15:11:25 +0300

    app for netlify

--- src/Pages/Register/components/RegisterPage.tsx
+++ src/Pages/Register/components/RegisterPage.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
-import { Header } from '../../../Widgets'
-
 const RegisterPage: FC = () => {
   return (
-    <div className="bg-blue-500">
-      <Header />
-    </div>
+    <form>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>sing up</button>
+    </form>
   )
 }
 
