759b14c2529ac2b40502869de09e15aac75d8aa0
Bondar-Sasha <Bondar-Sasha@users.noreply.github.com>
2025-02-11 19:41:57 +0000

    deploy: 8ba6e10028accc005531ef6c4a4cb2660b7f3266

--- assets/spinner.stories-N1ZISK--.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./jsx-runtime-z8MfsBtr.js";import{G as i}from"./iconBase-B85HJhYW.js";import"./index-C9rmetsa.js";import"./index-BofL8h0e.js";function c(r){return i({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none"},child:[{tag:"path",attr:{opacity:"0.2",fillRule:"evenodd",clipRule:"evenodd",d:"M12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19ZM12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z",fill:"currentColor"},child:[]},{tag:"path",attr:{d:"M2 12C2 6.47715 6.47715 2 12 2V5C8.13401 5 5 8.13401 5 12H2Z",fill:"currentColor"},child:[]}]})(r)}const a=({className:r=""})=>t.jsx("div",{className:r,children:t.jsx(c,{className:"animate-spin color-inherit"})});a.__docgenInfo={description:"",methods:[],displayName:"Spinner",props:{className:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"''",computed:!1}}}};const u={title:"Shared/loaders/basic",component:a,parameters:{layout:"centered"},tags:["autodocs","wip"],argTypes:{className:{control:"text",description:"Your customs styles for spinner.",table:{type:{}}}},args:{}},e={parameters:{docs:{description:{story:"Spinner showing."}}}};var s,o,n;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  parameters: {
-    docs: {
-      description: {
-        story: 'Spinner showing.'
-      }
-    }
-  }
-}`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const f=["Primary"];export{e as Primary,f as __namedExportsOrder,u as default};
--- assets/userLogo.stories-DsbY2oFq.js
+++
@@ -1,9 +0,0 @@
-import{j as c}from"./jsx-runtime-z8MfsBtr.js";import{G as i}from"./iconBase-B85HJhYW.js";import"./index-C9rmetsa.js";import"./index-BofL8h0e.js";function p(r){return i({tag:"svg",attr:{viewBox:"0 0 448 512"},child:[{tag:"path",attr:{d:"M313.6 304c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400 464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6 16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224 288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5 144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z"},child:[]}]})(r)}const a=({className:r="",onClick:n})=>c.jsx(p,{onClick:n,className:`cursor-pointer ${r}`});a.__docgenInfo={description:"",methods:[],displayName:"UserLogo",props:{className:{required:!1,tsType:{name:"string"},description:"",defaultValue:{value:"''",computed:!1}},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const g={title:"Shared/logos/UserLogo",component:a,parameters:{layout:"centered"},tags:["autodocs","wip"],argTypes:{className:{control:"text",description:"Your customs styles for logo.",table:{type:{}}}},args:{}},e={parameters:{docs:{description:{story:"logo showing."}}}};var o,s,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  parameters: {
-    docs: {
-      description: {
-        story: 'logo showing.'
-      }
-    }
-  }
-}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const y=["Primary"];export{e as Primary,y as __namedExportsOrder,g as default};
